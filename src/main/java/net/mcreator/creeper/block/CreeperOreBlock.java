469016f90ab49720370d5659e57838abe3bbdea7
BenGFly <BenGFly@DESKTOP-PBIVH6L>
2021-03-16 18:43:20 +0100

    +Leather Armor

--- src/main/java/net/mcreator/creeper/block/CreeperOreBlock.java
+++ src/main/java/net/mcreator/creeper/block/CreeperOreBlock.java
@@ -1,17 +1,45 @@
 
 package net.mcreator.creeper.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.feature.OreFeature;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.creeper.item.CreeperIngotItem;
+import net.mcreator.creeper.CreeperModElements;
+
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
 
 @CreeperModElements.ModElement.Tag
 public class CreeperOreBlock extends CreeperModElements.ModElement {
-
 	@ObjectHolder("creeper:creeper_ore")
 	public static final Block block = null;
-
 	public CreeperOreBlock(CreeperModElements instance) {
 		super(instance, 10);
-
 	}
 
 	@Override
@@ -20,45 +48,33 @@ public class CreeperOreBlock extends CreeperModElements.ModElement {
 		elements.items
 				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
 	}
-
 	public static class CustomBlock extends Block {
-
 		public CustomBlock() {
-			super(
-
-					Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(15f, 0f).lightValue(0).harvestLevel(4)
-							.harvestTool(ToolType.PICKAXE));
-
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(15f, 0f).lightValue(0).harvestLevel(4)
+					.harvestTool(ToolType.PICKAXE));
 			setRegistryName("creeper_ore");
 		}
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
 			return Collections.singletonList(new ItemStack(CreeperIngotItem.block, (int) (1)));
 		}
-
 	}
-
 	@Override
 	public void init(FMLCommonSetupEvent event) {
 		for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
-
 			biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, new OreFeature(OreFeatureConfig::deserialize) {
 				@Override
 				public boolean place(IWorld world, ChunkGenerator generator, Random rand, BlockPos pos, OreFeatureConfig config) {
 					DimensionType dimensionType = world.getDimension().getType();
 					boolean dimensionCriteria = false;
-
 					if (dimensionType == DimensionType.OVERWORLD)
 						dimensionCriteria = true;
-
 					if (!dimensionCriteria)
 						return false;
-
 					return super.place(world, generator, rand, pos, config);
 				}
 			}.withConfiguration(new OreFeatureConfig(OreFeatureConfig.FillerBlockType.create("creeper_ore", "creeper_ore", blockAt -> {
@@ -69,5 +85,4 @@ public class CreeperOreBlock extends CreeperModElements.ModElement {
 			}), block.getDefaultState(), 5)).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(10, 1, 1, 12))));
 		}
 	}
-
 }
--- src/main/java/net/mcreator/creeper/item/CreeperleatherarmorItem.java
+++ src/main/java/net/mcreator/creeper/item/CreeperleatherarmorItem.java
@@ -19,28 +19,28 @@ import net.minecraft.entity.Entity;
 import net.mcreator.creeper.CreeperModElements;
 
 @CreeperModElements.ModElement.Tag
-public class CreeperArmorItem extends CreeperModElements.ModElement {
-	@ObjectHolder("creeper:creeper_armor_helmet")
+public class CreeperleatherarmorItem extends CreeperModElements.ModElement {
+	@ObjectHolder("creeper:creeperleatherarmor_helmet")
 	public static final Item helmet = null;
-	@ObjectHolder("creeper:creeper_armor_chestplate")
+	@ObjectHolder("creeper:creeperleatherarmor_chestplate")
 	public static final Item body = null;
-	@ObjectHolder("creeper:creeper_armor_leggings")
+	@ObjectHolder("creeper:creeperleatherarmor_leggings")
 	public static final Item legs = null;
-	@ObjectHolder("creeper:creeper_armor_boots")
+	@ObjectHolder("creeper:creeperleatherarmor_boots")
 	public static final Item boots = null;
-	public CreeperArmorItem(CreeperModElements instance) {
-		super(instance, 6);
+	public CreeperleatherarmorItem(CreeperModElements instance) {
+		super(instance, 14);
 	}
 
 	@Override
 	public void initElements() {
 		IArmorMaterial armormaterial = new IArmorMaterial() {
 			public int getDurability(EquipmentSlotType slot) {
-				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 25;
+				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 7;
 			}
 
 			public int getDamageReductionAmount(EquipmentSlotType slot) {
-				return new int[]{4, 8, 9, 3}[slot.getIndex()];
+				return new int[]{1, 3, 5, 2}[slot.getIndex()];
 			}
 
 			public int getEnchantability() {
@@ -52,41 +52,41 @@ public class CreeperArmorItem extends CreeperModElements.ModElement {
 			}
 
 			public Ingredient getRepairMaterial() {
-				return Ingredient.fromStacks(new ItemStack(CreeperIngotItem.block, (int) (1)));
+				return Ingredient.fromStacks(new ItemStack(CreeperLeatherItem.block, (int) (1)));
 			}
 
 			@OnlyIn(Dist.CLIENT)
 			public String getName() {
-				return "creeper_armor";
+				return "creeperleatherarmor";
 			}
 
 			public float getToughness() {
-				return 3f;
+				return 0f;
 			}
 		};
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)) {
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
-				return "creeper:textures/models/armor/creeper_armor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+				return "creeper:textures/models/armor/creeperleatherarmor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-		}.setRegistryName("creeper_armor_helmet"));
+		}.setRegistryName("creeperleatherarmor_helmet"));
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)) {
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
-				return "creeper:textures/models/armor/creeper_armor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+				return "creeper:textures/models/armor/creeperleatherarmor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-		}.setRegistryName("creeper_armor_chestplate"));
+		}.setRegistryName("creeperleatherarmor_chestplate"));
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.LEGS, new Item.Properties().group(ItemGroup.COMBAT)) {
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
-				return "creeper:textures/models/armor/creeper_armor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+				return "creeper:textures/models/armor/creeperleatherarmor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-		}.setRegistryName("creeper_armor_leggings"));
+		}.setRegistryName("creeperleatherarmor_leggings"));
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)) {
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
-				return "creeper:textures/models/armor/creeper_armor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+				return "creeper:textures/models/armor/creeperleatherarmor_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
 			}
-		}.setRegistryName("creeper_armor_boots"));
+		}.setRegistryName("creeperleatherarmor_boots"));
 	}
 }
