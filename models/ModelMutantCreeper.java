479e37579ed74748dcd7520f46b428eb89bd0595
BenGFly <BenGFly@DESKTOP-PBIVH6L>
2021-03-20 06:21:28 +0100

    +Modelo del mob mutante cambiado a estatura normal de creeper con nueva textura

---
+++ models/ModelMutantCreeper.java
@@ -0,0 +1,83 @@
+// Made with Blockbench 3.8.2
+// Exported for Minecraft version 1.15
+// Paste this class into your mod and generate all required imports
+
+public static class ModelMutantCreeper extends EntityModel<Entity> {
+	private final ModelRenderer head3;
+	private final ModelRenderer armor;
+	private final ModelRenderer body;
+	private final ModelRenderer leg1;
+	private final ModelRenderer leg2;
+	private final ModelRenderer leg3;
+	private final ModelRenderer leg4;
+	private final ModelRenderer head;
+	private final ModelRenderer head2;
+
+	public ModelMutantCreeper() {
+		textureWidth = 64;
+		textureHeight = 64;
+
+		head3 = new ModelRenderer(this);
+		head3.setRotationPoint(0.0F, 6.0F, 0.0F);
+		head3.setTextureOffset(24, 24).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, 0.0F, false);
+
+		armor = new ModelRenderer(this);
+		armor.setRotationPoint(0.0F, 24.0F, 0.0F);
+
+		body = new ModelRenderer(this);
+		body.setRotationPoint(0.0F, 6.0F, 0.0F);
+		body.setTextureOffset(0, 32).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, 0.0F, false);
+
+		leg1 = new ModelRenderer(this);
+		leg1.setRotationPoint(2.0F, 18.0F, 4.0F);
+		leg1.setTextureOffset(40, 40).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+
+		leg2 = new ModelRenderer(this);
+		leg2.setRotationPoint(-2.0F, 18.0F, 4.0F);
+		leg2.setTextureOffset(24, 40).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+
+		leg3 = new ModelRenderer(this);
+		leg3.setRotationPoint(2.0F, 18.0F, -4.0F);
+		leg3.setTextureOffset(32, 0).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+
+		leg4 = new ModelRenderer(this);
+		leg4.setRotationPoint(-2.0F, 18.0F, -4.0F);
+		leg4.setTextureOffset(28, 12).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+
+		head = new ModelRenderer(this);
+		head.setRotationPoint(-9.0F, 22.0F, 0.0F);
+		setRotationAngle(head, 0.0F, 0.0F, 0.5236F);
+		head.setTextureOffset(0, 16).addBox(2.0F, -27.0F, -4.0F, 8.0F, 8.0F, 8.0F, 0.0F, false);
+
+		head2 = new ModelRenderer(this);
+		head2.setRotationPoint(-4.0F, 30.0F, 0.0F);
+		setRotationAngle(head2, 0.0F, 0.0F, -0.5236F);
+		head2.setTextureOffset(0, 0).addBox(6.0F, -27.0F, -4.0F, 8.0F, 8.0F, 8.0F, 0.0F, false);
+	}
+
+	@Override
+	public void setRotationAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks,
+			float netHeadYaw, float headPitch) {
+		// previously the render function, render code was moved to a method below
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red,
+			float green, float blue, float alpha) {
+		head3.render(matrixStack, buffer, packedLight, packedOverlay);
+		armor.render(matrixStack, buffer, packedLight, packedOverlay);
+		body.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg1.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg2.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg3.render(matrixStack, buffer, packedLight, packedOverlay);
+		leg4.render(matrixStack, buffer, packedLight, packedOverlay);
+		head.render(matrixStack, buffer, packedLight, packedOverlay);
+		head2.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+}
--- src/main/java/net/mcreator/creeper/entity/BigCreeperEntity.java
+++ src/main/java/net/mcreator/creeper/entity/BigCreeperEntity.java
@@ -78,10 +78,10 @@ public class BigCreeperEntity extends CreeperModElements.ModElement {
 	@OnlyIn(Dist.CLIENT)
 	public void registerModels(ModelRegistryEvent event) {
 		RenderingRegistry.registerEntityRenderingHandler(entity, renderManager -> {
-			return new MobRenderer(renderManager, new ModelCreeperMutant(), 0.5f) {
+			return new MobRenderer(renderManager, new ModelMutantCreeper(), 0.5f) {
 				@Override
 				public ResourceLocation getEntityTexture(Entity entity) {
-					return new ResourceLocation("creeper:textures/creepermutantmini.png");
+					return new ResourceLocation("creeper:textures/newtexture.png");
 				}
 			};
 		});
@@ -170,32 +170,47 @@ public class BigCreeperEntity extends CreeperModElements.ModElement {
 	// Made with Blockbench 3.8.2
 	// Exported for Minecraft version 1.15
 	// Paste this class into your mod and generate all required imports
-	public static class ModelCreeperMutant extends EntityModel<Entity> {
-		private final ModelRenderer Mob;
-		private final ModelRenderer cube_r1;
-		private final ModelRenderer cube_r2;
-		public ModelCreeperMutant() {
+	public static class ModelMutantCreeper extends EntityModel<Entity> {
+		private final ModelRenderer head3;
+		private final ModelRenderer armor;
+		private final ModelRenderer body;
+		private final ModelRenderer leg1;
+		private final ModelRenderer leg2;
+		private final ModelRenderer leg3;
+		private final ModelRenderer leg4;
+		private final ModelRenderer head;
+		private final ModelRenderer head2;
+		public ModelMutantCreeper() {
 			textureWidth = 64;
 			textureHeight = 64;
-			Mob = new ModelRenderer(this);
-			Mob.setRotationPoint(0.0F, 24.0F, 0.0F);
-			Mob.setTextureOffset(0, 30).addBox(-9.0F, -6.0F, 0.0F, 6.0F, 6.0F, 6.0F, 0.0F, false);
-			Mob.setTextureOffset(6, 13).addBox(-4.0F, -15.0F, -3.5F, 6.0F, 12.0F, 6.0F, 0.0F, false);
-			Mob.setTextureOffset(0, 30).addBox(0.0F, -6.0F, 0.0F, 6.0F, 6.0F, 6.0F, 0.0F, false);
-			Mob.setTextureOffset(0, 30).addBox(0.0F, -6.0F, -7.0F, 6.0F, 6.0F, 6.0F, 0.0F, false);
-			Mob.setTextureOffset(0, 30).addBox(-2.0F, -16.0F, -1.5F, 2.0F, 1.0F, 2.0F, 0.0F, false);
-			Mob.setTextureOffset(0, 30).addBox(-4.0F, -22.0F, -3.5F, 6.0F, 6.0F, 6.0F, 0.0F, false);
-			Mob.setTextureOffset(0, 30).addBox(-9.0F, -6.0F, -7.0F, 6.0F, 6.0F, 6.0F, 0.0F, false);
-			cube_r1 = new ModelRenderer(this);
-			cube_r1.setRotationPoint(0.0F, 0.0F, 0.0F);
-			Mob.addChild(cube_r1);
-			setRotationAngle(cube_r1, 0.6109F, 0.0F, 0.0F);
-			cube_r1.setTextureOffset(0, 30).addBox(-4.0F, -19.0F, 0.5F, 6.0F, 6.0F, 6.0F, 0.0F, false);
-			cube_r2 = new ModelRenderer(this);
-			cube_r2.setRotationPoint(0.0F, 0.0F, 0.0F);
-			Mob.addChild(cube_r2);
-			setRotationAngle(cube_r2, -0.4363F, 0.0F, 0.0F);
-			cube_r2.setTextureOffset(0, 30).addBox(-4.0F, -19.0F, -4.5F, 6.0F, 6.0F, 6.0F, 0.0F, false);
+			head3 = new ModelRenderer(this);
+			head3.setRotationPoint(0.0F, 6.0F, 0.0F);
+			head3.setTextureOffset(24, 24).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, 0.0F, false);
+			armor = new ModelRenderer(this);
+			armor.setRotationPoint(0.0F, 24.0F, 0.0F);
+			body = new ModelRenderer(this);
+			body.setRotationPoint(0.0F, 6.0F, 0.0F);
+			body.setTextureOffset(0, 32).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, 0.0F, false);
+			leg1 = new ModelRenderer(this);
+			leg1.setRotationPoint(2.0F, 18.0F, 4.0F);
+			leg1.setTextureOffset(40, 40).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+			leg2 = new ModelRenderer(this);
+			leg2.setRotationPoint(-2.0F, 18.0F, 4.0F);
+			leg2.setTextureOffset(24, 40).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+			leg3 = new ModelRenderer(this);
+			leg3.setRotationPoint(2.0F, 18.0F, -4.0F);
+			leg3.setTextureOffset(32, 0).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+			leg4 = new ModelRenderer(this);
+			leg4.setRotationPoint(-2.0F, 18.0F, -4.0F);
+			leg4.setTextureOffset(28, 12).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 6.0F, 4.0F, 0.0F, false);
+			head = new ModelRenderer(this);
+			head.setRotationPoint(-9.0F, 22.0F, 0.0F);
+			setRotationAngle(head, 0.0F, 0.0F, 0.5236F);
+			head.setTextureOffset(0, 16).addBox(2.0F, -27.0F, -4.0F, 8.0F, 8.0F, 8.0F, 0.0F, false);
+			head2 = new ModelRenderer(this);
+			head2.setRotationPoint(-4.0F, 30.0F, 0.0F);
+			setRotationAngle(head2, 0.0F, 0.0F, -0.5236F);
+			head2.setTextureOffset(0, 0).addBox(6.0F, -27.0F, -4.0F, 8.0F, 8.0F, 8.0F, 0.0F, false);
 		}
 
 		@Override
@@ -206,7 +221,15 @@ public class BigCreeperEntity extends CreeperModElements.ModElement {
 		@Override
 		public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue,
 				float alpha) {
-			Mob.render(matrixStack, buffer, packedLight, packedOverlay);
+			head3.render(matrixStack, buffer, packedLight, packedOverlay);
+			armor.render(matrixStack, buffer, packedLight, packedOverlay);
+			body.render(matrixStack, buffer, packedLight, packedOverlay);
+			leg1.render(matrixStack, buffer, packedLight, packedOverlay);
+			leg2.render(matrixStack, buffer, packedLight, packedOverlay);
+			leg3.render(matrixStack, buffer, packedLight, packedOverlay);
+			leg4.render(matrixStack, buffer, packedLight, packedOverlay);
+			head.render(matrixStack, buffer, packedLight, packedOverlay);
+			head2.render(matrixStack, buffer, packedLight, packedOverlay);
 		}
 
 		public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
